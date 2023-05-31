05bd56364f7c9c2505caa155a0e8097a1e9d0dc6
alexeyzverev <alexey.zverev@exactpro.com>
2023-05-31 17:43:33 +0100

    no implieds in l3 update fix3

--- recon_lw/recon_ob.py
+++ recon_lw/recon_ob.py
@@ -696,6 +696,33 @@ def ob_indicative_open_price(opening_price: float, str_time_of_event, order_book
     return {}, [copy.deepcopy(order_book)]
 
 
+def is_side_unchanged(side, new_price, new_real_qty, new_impl_qty, new_real_n_orders, new_impl_n_orders, book):
+    if new_price is None:
+        return book[side+"_price"] is None
+    if book[side+"_price"] is None:
+        return False
+    return book[side+"_price"] == new_price and \
+           book[side+"_real_qty"] == new_real_qty and \
+           book[side+"_impl_qty"] == new_impl_qty and \
+           book[side+"_real_n_orders"] == new_real_n_orders and \
+           book[side+"_impl_n_orders"] == new_impl_n_orders
+
+
+def is_side_update_purely_implied(side, new_price, new_real_qty, new_impl_qty,
+                                  new_real_n_orders, new_impl_n_orders, book):
+    reverse = -1 if side == "bid" else 1
+    if new_price is None:  # deleted level
+        return book[side+"_real_n_orders"] == 0
+    elif book[side+"_price"] is None:  # added level to empty book
+        return new_real_n_orders == 0
+    if book[side+"_price"] == new_price:  # top level update
+        return new_real_n_orders == book[side+"_real_n_orders"] and book[side+"_real_qty"] == new_real_qty
+    if reverse * book[side+"_price"] < reverse * new_price:  # price is better - new level is added
+        return new_real_n_orders == 0
+    else:  # price is worse old level is deleted
+        return book[side+"_real_n_orders"] == 0
+
+
 def ob_top_update(ask_price: float, ask_real_qty: int, ask_impl_qty: int, ask_real_n_orders: int, ask_impl_n_orders: int,
                   bid_price: float, bid_real_qty: int, bid_impl_qty: int, bid_real_n_orders: int, bid_impl_n_orders: int,
                   str_time_of_event, order_book: dict) -> tuple:
@@ -704,33 +731,17 @@ def ob_top_update(ask_price: float, ask_real_qty: int, ask_impl_qty: int, ask_re
     else:
         reset_aggr_seq(order_book)
 
-    #is it purely implied
-    sell_implieds_only = False
-    if ask_price == order_book["ask_price"]:
-        if ask_real_n_orders == order_book["ask_real_n_orders"]:
-            sell_implieds_only = True
-    if ask_price > order_book["ask_price"]:
-        if ask_impl_n_orders != 0 and \
-                ask_real_n_orders == 0:
-            sell_implieds_only = True
-    if ask_price < order_book["ask_price"]:
-        if order_book["ask_impl_n_orders"] != 0 and \
-                order_book["ask_real_n_orders"] == 0:
-            sell_implieds_only = True
+    ask_unchanged = is_side_unchanged("ask", ask_price, ask_real_qty, ask_impl_qty,
+                                      ask_real_n_orders, ask_impl_n_orders, order_book)
+    ask_implied_only = is_side_update_purely_implied("ask", ask_price, ask_real_qty, ask_impl_qty,
+                                                     ask_real_n_orders, ask_impl_n_orders, order_book)
+    bid_unchanged = is_side_unchanged("bid", bid_price, bid_real_qty, bid_impl_qty,
+                                      bid_real_n_orders, bid_impl_n_orders, order_book)
+    bid_implied_only = is_side_update_purely_implied("bid", bid_price, bid_real_qty, bid_impl_qty,
+                                                     bid_real_n_orders, bid_impl_n_orders, order_book)
 
-    buy_implieds_only = False
-    if bid_price == order_book["bid_price"]:
-        if bid_real_n_orders == order_book["bid_real_n_orders"]:
-            buy_implieds_only = True
-    if bid_price < order_book["bid_price"]:
-        if bid_impl_n_orders != 0 and \
-                bid_real_n_orders == 0:
-            buy_implieds_only = True
-    if bid_price < order_book["bid_price"]:
-        if order_book["bid_impl_n_orders"] != 0 and \
-                order_book["bid_real_n_orders"] == 0:
-            buy_implieds_only = True
-    order_book["implied_only"] = sell_implieds_only and buy_implieds_only
+    order_book["implied_only"] = (ask_implied_only and bid_unchanged)\
+                                 or (bid_implied_only and ask_unchanged) or (bid_implied_only and ask_implied_only)
 
     order_book["ask_price"] = ask_price
     order_book["ask_real_qty"] = ask_real_qty
