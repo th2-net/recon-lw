6b0ea159b1ea731175e873ef60fbd4a5b7f44137
Slava Ermakov <connect-soft@yandex.ru>
2023-08-28 12:59:12 +0500

    Moved message_to_dict from DS-core to recon-lw

---
+++ recon_lw/message_utils.py
@@ -0,0 +1,60 @@
+#  Copyright 2023 Exactpro (Exactpro Systems Limited)
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+from th2_data_services.config import options
+from th2_data_services.utils.converters import flatten_dict
+
+
+def message_to_dict(message: dict):
+    """Converts message body.fields to flatten dict.
+
+    Will return message["simpleBody"] is "simpleBody" in message.
+    "simpleBody" -- recon-lw field, that contains flatten dict.
+
+    This function expects the message will be without list in the body.
+    So expected message format is:
+        {
+        ..
+        body: {fields: {}, metadata: {}}
+        }
+
+    Args:
+        message: TH2-Message
+
+    Returns:
+        Dict
+    """
+    # The function was moved from DS-core because it has recon-lw business logic.
+
+    if "simpleBody" in message:
+        return message["simpleBody"]
+
+    # TODO - Will work for old-msgs format, but not for Lwdp-3
+    #   because LwDP3 messages have another structure of the body.
+    #   Body is a list of dicts.
+    try:
+        # TODO - it'll bring problems if the message body doesn't have ["fields"].
+        result = flatten_dict(options.MESSAGE_FIELDS_RESOLVER.get_body(message)["fields"])
+        return result
+    except Exception:
+        print(
+            """message_to_dict function expects the message will be without list in the body.
+                So expected message format is:
+                    {
+                    ..
+                    body: {fields: {}, metadata: {}}
+                    }
+                    """
+        )
+        print(f"Got the message: {message}")
+        raise
