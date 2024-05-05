602497b1f261f3948acdba28b8179fb71625789f
Slava Ermakov <connect-soft@yandex.ru>
2024-05-05 14:00:15 +0500

    Fix typing

--- recon_lw/interpretation/field_extractor/base.py
+++ recon_lw/interpretation/field_extractor/base.py
@@ -1,7 +1,13 @@
-from recon_lw.interpretation.adapter.base import Adapter
+from __future__ import annotations
 from recon_lw.core.type.types import Message
 from typing import Optional, Any, Protocol, runtime_checkable
-from abc import ABC, abstractmethod
+from abc import ABC
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from recon_lw.interpretation.adapter.base import Adapter
+
 
 class Extractor(ABC):
     """
@@ -17,6 +23,7 @@ class Extractor(ABC):
 
     def extract(self, message: Message, adapter: Adapter) -> Optional[Any]:
         pass
+
 
 class ExtractorProtocol(Protocol):
     def __call__(self, message: Message, adapter: Adapter):
