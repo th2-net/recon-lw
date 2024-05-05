bf88295962a94e02c6520af5b02f631f91b856a1
Slava Ermakov <connect-soft@yandex.ru>
2024-05-05 14:04:32 +0500

    Refactor MatchingKeyExtractor

--- recon_lw/matching/matching_key_extractor/base.py
+++ recon_lw/matching/matching_key_extractor/base.py
@@ -2,15 +2,32 @@ from abc import ABC, abstractmethod
 from typing import Any, Dict, Set, List, Protocol
 from recon_lw.interpretation.adapter.base import Adapter
 
-class MatchingKeyExtractor(ABC):
 
-    def __call__(self, adapter: Adapter, message: Dict[str, Any], fields: Set[str]) -> List[str]:
+class IMatchingKeyExtractor(ABC):
+
+    def __call__(self, adapter: Adapter, message: Dict[str, Any],
+                 fields: Set[str]) -> List[str]:
         return self.extract(adapter, message, fields)
 
     @abstractmethod
-    def extract(self, adapter: Adapter, message: Dict[str, Any], fields: Set[str]) -> List[str]:
+    def extract(self, adapter: Adapter, message: Dict[str, Any],
+                fields: Set[str]) -> List[str]:
         pass
 
+
 class MatchingKeyExtractorProtocol(Protocol):
-    def __call__(self, adapter: Adapter, message: Dict[str, Any], fields: Set[str]) -> List[str]:
-        pass
+    """
+    The callable object to define `Matching key` for the message.
+    """
+    def __call__(self, adapter: Adapter, message: Dict[str, Any],
+                 fields: Set[str]) -> List[str]:
+        """
+
+        Args:
+            adapter:
+            message:
+            fields: Matching fields
+
+        Returns:
+
+        """
